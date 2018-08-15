f1bf5346369d33e19bd2ec2419cb87b82f62dcbe
Pavel Kotrc <pkotrc@gmail.com>
2018-08-15 11:11:32 +0200

    Delete existing merge branch

--- git_merge_check.sh
+++ git_merge_check.sh
@@ -8,6 +8,16 @@ fi
 exitStatus=0
 MERGE_BRANCH="master-merge-dry-run"
 
+git rev-parse --verify --quiet $MERGE_BRANCH > /dev/null
+if [ $? -eq 0 ]; then
+	echo "Deleting existing branch '$MERGE_BRANCH'"
+	git branch -D $MERGE_BRANCH
+	if [ $? -ne 0 ]; then
+		echo "Branch deletion failed; exiting"
+		exit 1
+	fi
+fi
+
 if [ $# -eq 1 ]; then
    git checkout -b $MERGE_BRANCH $1 > /dev/null 2>&1
 else
